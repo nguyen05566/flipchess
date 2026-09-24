bc15e0a54acb0172dd8a5118f3c56ee8f3d65be1
nguyen05566 <javjp606@gmail.com>
2026-09-25 03:45:45 +0700

    Update arena20.py

--- arena20.py
+++ arena20.py
@@ -103,9 +103,9 @@ KICK_MODE = "when_lose"
 KICK_DELAY = 5.0
 
 # ==================== TÌM BÀN / TẠO BÀN ====================
-BET_MIN = 5000
+BET_MIN = 10000
 BET_MAX = 100000
-BOT_BET_XU = 5000
+BOT_BET_XU = 10000
 QUICK_PLAY_MAX_ATTEMPTS = 8               # dò tối đa 8 lần MỖI lobby
 BOT_USE_CREATE_TABLE = False
 
